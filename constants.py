dc400d238416ce204a57107785b25660bbe79269
arya-g-rao <68414093+arya-g-rao@users.noreply.github.com>
2022-07-27 13:29:45 -0400

    CSCI-620 - Updated code for rfm analysis.

--- constants.py
+++ constants.py
@@ -16,3 +16,14 @@ RECENCY_LABELS = [1, 2, 3, 4]
 
 BASE_MONETARY_BINS = [0, 2, 5, 10]
 MONETARY_LABELS = [1, 2, 3, 4]
+
+CATEGORY_MAP = {'Champion': {'RECENCY_SCORE': ['2', '3', '4'], 'FREQUENCY_SCORE': ['4'], 'MONETARY_SCORE': ['4']},
+                'Top Loyal Customer': {'RECENCY_SCORE': ['3'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['3', '4']},
+                'Loyal Customer': {'RECENCY_SCORE': ['3'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['1', '2']},
+                'Top Recent Customer': {'RECENCY_SCORE': ['4'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['3', '4']},
+                'Recent Customer': {'RECENCY_SCORE': ['4'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['1', '2']},
+                'Top Customer Needed Attention': {'RECENCY_SCORE': ['2', '3'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['3', '4']},
+                'Customer Needed Attention': {'RECENCY_SCORE': ['2', '3'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['1', '2']},
+                'Top Lost Customer': {'RECENCY_SCORE': ['1'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['3', '4']},
+                'Lost Customer': {'RECENCY_SCORE': ['1'], 'FREQUENCY_SCORE': ['1', '2', '3', '4'], 'MONETARY_SCORE': ['1', '2']},
+                }
--- customer_segmentation.py
+++ customer_segmentation.py
@@ -1,3 +1,6 @@
+from constants import CATEGORY_MAP
+
+
 def get_high_level_customer_info(data_df):
     """
     Function to get the high level customer information
@@ -16,6 +19,25 @@ def allocate_total_rfm_score(data_df):
     """
     data_df['TOTAL_RFM_SCORE'] = data_df.apply(lambda x: str(int(x['RECENCY_SCORE'])) + str(int(x['FREQUENCY_SCORE'])) + str(int(x['MONETARY_SCORE'])), axis=1)
     data_df['TOTAL_RFM_LABEL'] = data_df['RECENCY_SCORE'] + data_df['FREQUENCY_SCORE'] + data_df['MONETARY_SCORE']
+    return data_df
+
+
+def categorizer(row):
+
+    for key in CATEGORY_MAP:
+        score_map = CATEGORY_MAP[key]
+        if row['RECENCY_SCORE'] in score_map['RECENCY_SCORE'] and row['FREQUENCY_SCORE'] in score_map['FREQUENCY_SCORE'] and row['MONETARY_SCORE'] in score_map['MONETARY_SCORE']:
+            return key
+
+    return 'NOT_CATEGORIZED'
+
+
+def get_high_level_customer_segmentation_info(data_df):
+    """
+    Function to do something.
+    :param data_df:
+    :return:
+    """
     return data_df
 
 
